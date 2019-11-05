3b842d4afd7e031831f81b9e6bae208028e9116f
67sivad <mdwilliams67@gmail.com>
2019-11-04 23:11:34 -0500

    Redid app with Storyboard instead of SwiftUI

---
+++ CampusSafari/CampusSafari/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CampusSafari
+//
+//  Created by Davis Williams on 11/4/19.
+//  Copyright © 2019 Team 9323. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
