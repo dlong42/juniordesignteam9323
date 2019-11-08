bbb1943989767fb3a4b81af84eabb2c7670c6850
Devon Long <dlong42@gatech.edu>
2019-11-08 13:43:12 -0500

    introduce map and map boundaries

--- CampusSafari/CampusSafari/ViewController.swift
+++ CampusSafari/CampusSafari/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    let regionRadius: CLLocationDistance = 1000
+    let coordBoundary = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 33.7765, longitude: -84.3983), latitudinalMeters: 939, longitudinalMeters: 1715)
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let initialLocation = CLLocation(latitude: 33.7765, longitude: -84.3983)
+        centerMapOnLocation(location: initialLocation)
+        mapView.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: coordBoundary), animated: true)
     }
-
-
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
 
