ba195c795cf214f851281be2ee27acaaef6ae2e6
dlong42 <dlong42@gatech.edu>
2020-01-11 14:26:49 -0500

    create multiplatform project

---
+++ CampusSafariMultiPlatform/CampusSafariMultiPlatform/CampusSafariMultiPlatform/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CampusSafariMultiPlatform.Models;
+
+namespace CampusSafariMultiPlatform.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
