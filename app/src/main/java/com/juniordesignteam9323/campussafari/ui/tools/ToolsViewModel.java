25e383e163d03c8921fe5a65eb7ccaf627dddf63
dlong42 <dlong42@gatech.edu>
2020-01-11 19:15:35 -0500

    create project in android studio

---
+++ app/src/main/java/com/juniordesignteam9323/campussafari/ui/tools/ToolsViewModel.java
@@ -0,0 +1,19 @@
+package com.juniordesignteam9323.campussafari.ui.tools;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ToolsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ToolsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is tools fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
