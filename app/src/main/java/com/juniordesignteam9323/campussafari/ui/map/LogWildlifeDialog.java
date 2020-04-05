9ae64d1df9948878831585d228386fdb171f9ea0
Long <dlong42-gtri@csr-ast-l01.attlocal.net>
2020-04-05 00:09:17 -0400

    created shell for log wildlife dialog

---
+++ app/src/main/java/com/juniordesignteam9323/campussafari/ui/map/LogWildlifeDialog.java
@@ -0,0 +1,7 @@
+package com.juniordesignteam9323.campussafari.ui.map;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class LogWildlifeDialog extends AppCompatDialogFragment {
+    
+}
